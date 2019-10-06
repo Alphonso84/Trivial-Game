d40154aae039de1fbbc35179e9992ef81d1892ac
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2019-10-06 15:33:53 -0700

    Initial Commit

---
+++ Tests UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tests UI
+//
+//  Created by Alphonso Sensley II on 10/6/19.
+//  Copyright © 2019 Alphonso Sensley II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
